e663f5a49e178b380ff220b8ddd315904bb0f285
Wycliffe Kerich <dev.kerich@gmail.com>
2022-06-30 13:40:14 -0700

    Print a text to std output

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -9,7 +9,7 @@
 
 int main(void)
 {
-  char str[] = "\"Programming is like building a multilingual puzzle";
-  puts(str);
-  return (0);
+char str[] = "\"Programming is like building a multilingual puzzle";
+puts(str);
+return (0);
 }
